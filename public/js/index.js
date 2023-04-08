e0a58a268659f5e120f49709ce43442ad7be9c12
Mariano <marianotrogo@gmail.com>
2023-04-08 16:11:08 -0300

    rout

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,44 @@
 const socket = io();
 
-const productData = document.getProductsById("prodsList-display");
-socket.on("productList", async(data)=>{
-    console.log(data);
-    let prodsList = "";
-    await data.forEach((e)=>{
-        prodsList += `
-            <ul>
-                <li> Titulo:${e.title},
-                    Id:${e.id},
-                    Precio:${e.precio},
-                    Stock:${e.stock}
-                </li>
-            </ul>`
-        });
-        productData.innerHTML = prodsList;
+const table = document.getElementById("product-table");
+socket.on("NewActionWithProduct", (products) => {
+  console.log(products)
+  table.innerHTML = `<tr>
+                      <th style="width: 10%;"> title</th>
+                      <th style="width: 10%;"> description</th>
+                      <th style="width: 10%;"> code</th>
+                      <th style="width: 10%;"> price</th>
+                      <th style="width: 10%;"> stock</th>
+                      <th style="width: 10%;"> thumbnails</th>
+                      <th style="width: 10%;"> Id</th>
+                    </tr>`;
+
+  products.forEach((product) => {
+    let newRow = document.createElement("tr");
+    Object.values(product).forEach((value) => {
+      let cell = document.createElement("td");
+      cell.innerText = value;
+      newRow.appendChild(cell);
+    });
+    table.appendChild(newRow);
+  });
 });
+
+// const socket = io();
+
+// const productData = document.getProductsById("prodsList-display");
+// socket.on("productList", async(data)=>{
+//     console.log(data);
+//     let prodsList = "";
+//     await data.forEach((e)=>{
+//         prodsList += `
+//             <ul>
+//                 <li> Titulo:${e.title},
+//                     Id:${e.id},
+//                     Precio:${e.precio},
+//                     Stock:${e.stock}
+//                 </li>
+//             </ul>`
+//         });
+//         productData.innerHTML = prodsList;
+// });
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,18 +1,38 @@
-import { Router, json } from "express";
-import productManager from "../productManager.js";
+import { Router } from "express";
+import ProductManager from "../productManager.js";
+import CartManager from "../cartManager/cartManager.js";
 
-const item = new productManager();
 
 const viewsRouter = Router();
+const productManager = new ProductManager();
+viewsRouter.get("/", async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render("home",{products});
+});
 
-viewsRouter.get("/", async(req,res)=>{
-    const prods =await item.getProducts();
-    console.log(prods);
-    res.render("home",{prods});
-})
+viewsRouter.get("/realTimeProducts", async (req, res) => {
+  const products = await productManager.getProducts();
+  res.render("realTimeProducts", { products });
+});
 
-viewsRouter.get('/realTimeProducts', (req,res)=>{
-    res.render('realTimeProducts');
-})
 
 export default viewsRouter;
+
+// import { Router, json } from "express";
+// import productManager from "../productManager.js";
+
+// const item = new productManager();
+
+// const viewsRouter = Router();
+
+// viewsRouter.get("/", async(req,res)=>{
+//     const prods =await item.getProducts();
+//     console.log(prods);
+//     res.render("home",{prods});
+// })
+
+// viewsRouter.get('/realTimeProducts', (req,res)=>{
+//     res.render('realTimeProducts');
+// })
+
+// export default viewsRouter;
