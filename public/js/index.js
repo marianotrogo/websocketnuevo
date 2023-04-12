40d0256b8db4009cf32cae73f36073ea14a6c562
Mariano <marianotrogo@gmail.com>
2023-04-12 13:12:03 -0300

    cambios

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,41 @@
-const socket = io();
+const socket = io()
 
-const table = document.getElementById("product-table");
-socket.on("NewActionWithProduct", (products) => {
-  console.log(products)
-  table.innerHTML = `<tr>
-                      <th style="width: 10%;"> title</th>
-                      <th style="width: 10%;"> description</th>
-                      <th style="width: 10%;"> code</th>
-                      <th style="width: 10%;"> price</th>
-                      <th style="width: 10%;"> stock</th>
-                      <th style="width: 10%;"> thumbnails</th>
-                      <th style="width: 10%;"> Id</th>
-                    </tr>`;
+const containerProducts = document.getElementById("product-table")
 
-  products.forEach((product) => {
-    let newRow = document.createElement("tr");
-    Object.values(product).forEach((value) => {
-      let cell = document.createElement("td");
-      cell.innerText = value;
-      newRow.appendChild(cell);
-    });
-    table.appendChild(newRow);
-  });
-});
+socket.on("new-product", (data)=>{
+    containerProducts.innerHTML += `
+                                    <li>
+                                        <p><b>${data.title}</b></p>
+                                        <p>Precio: $ ${data.price}</p>
+                                        <p>Stock: ${data.stock}</p>
+                                        <p>Categoría: ${data.category}</p>
+                                    </li>
+                                    `
+})
+
+
+// const socket = io();
+
+// const table = document.getElementById("product-table");
+// socket.on("NewActionWithProduct", (products) => {
+//   console.log(products)
+//   table.innerHTML = `<tr>
+//                       <th style="width: 10%;"> title</th>
+//                       <th style="width: 10%;"> description</th>
+//                       <th style="width: 10%;"> code</th>
+//                       <th style="width: 10%;"> price</th>
+//                       <th style="width: 10%;"> stock</th>
+//                       <th style="width: 10%;"> thumbnails</th>
+//                       <th style="width: 10%;"> Id</th>
+//                     </tr>`;
+
+//   products.forEach((product) => {
+//     let newRow = document.createElement("tr");
+//     Object.values(product).forEach((value) => {
+//       let cell = document.createElement("td");
+//       cell.innerText = value;
+//       newRow.appendChild(cell);
+//     });
+//     table.appendChild(newRow);
+//   });
+// });
